16bd2d880d2cd3ddc585d5ddcf2602c7884d9191
Joao Vitor Guilherme dos Santos <j247232@dac.unicamp.br>
2025-06-12 14:34:39 -0300

    feat: adiciona atributo id do tipo UUID na classe transacao

--- app/src/main/java/planejadorfinanceiro/Transacao.java
+++ app/src/main/java/planejadorfinanceiro/Transacao.java
@@ -1,21 +1,26 @@
 package planejadorfinanceiro;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class Transacao {
 
-    public Transacao(double valor, String nome, TipoTransacao tipo, LocalDate data) {
-        this.valor = valor;
-        this.nome = nome;
-        this.tipo = tipo;
-        this.data = data;
-    }
-
+    private UUID id;
     private double valor;
     private String nome;
     private TipoTransacao tipo;
     private LocalDate data;
 
+    public Transacao(double valor, String nome, TipoTransacao tipo, LocalDate data) {
+        this.id = UUID.randomUUID();
+        this.valor = valor;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.data = data;
+    }
+    public UUID getId(){
+        return id;
+    }
     public String getNome() {
         return nome;
     }
