4e848534cbf34ae2be98ab5614c29a17b62bbad1
Gabriel Pinto Costa <gabrielcostah13@gmail.com>
2025-06-15 21:42:49 -0300

    adicionando o uso de UUID para identificar cada transação com um id único

--- app/src/main/java/planejadorfinanceiro/model/Transacao.java
+++ app/src/main/java/planejadorfinanceiro/model/Transacao.java
@@ -1,9 +1,11 @@
 package planejadorfinanceiro.model;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class Transacao {
 
+    private UUID id;
     private double valor;
     private String nome;
     private TipoTransacao tipo;
@@ -11,13 +13,20 @@ public class Transacao {
     
     // Construtor padrão necessário para deserialização JSON
     public Transacao() {
+        this.id = UUID.randomUUID(); // Garante que mesmo ao deserializar, haja um ID
     }
 
     public Transacao(double valor, String nome, TipoTransacao tipo, LocalDate data) {
+        this.id = UUID.randomUUID();
         this.valor = valor;
         this.nome = nome;
         this.tipo = tipo;
         this.data = data;
+    }
+
+    // Getters
+    public UUID getId() {
+        return id;
     }
 
     public String getNome() {
